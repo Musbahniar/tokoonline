bc754438ef2c7cbfcdb57c1c72554d18b75a65e8
Musbahniar <ruang.9campus@gmail.com>
2018-11-18 11:05:07 +0700

    commit all

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express  = require('express');
 var path  = require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
 
 // Membuat Inisial App
 var app  = express();
@@ -11,6 +13,17 @@ app.set('view engine','ejs');
 //Setup Public
 app.use(express.static(path.join(__dirname,'public')));
 
+// Connect to db
+mongoose.connect(config.database);
+var db = mongoose.connection;
+// mongoose.connect('mongodb://localhost/toko-onlinesalah');
+db.on('error', console.error.bind(console, 'Connection Error:'));
+
+db.once('open',function(){
+    console.log('Connection Open');
+})
+
+
 //Set Server
 var port = 3000;
 app.listen(port, function() {
@@ -18,5 +31,5 @@ app.listen(port, function() {
 });
 
 app.get('/', function(req,res){
-    res.send('Ini adalah Index x');
+    res.send('Ini adalah Index');
 });
