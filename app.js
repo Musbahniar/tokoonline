1c2e3652cca7911f730bece6767b4c2d910482d4
Musbahniar <ruang.9campus@gmail.com>
2018-11-18 14:59:48 +0700

    add bodyparser-expSession-expMessage

--- app.js
+++ app.js
@@ -2,9 +2,49 @@ var express  = require('express');
 var path  = require('path');
 var mongoose = require('mongoose');
 var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
 
 // Membuat Inisial App
 var app  = express();
+
+//Set MidlleWare Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+//Set Middleware Express-Session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    //cookie: { secure: true }
+  }));
+
+//Set Middleware Express-Validator
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+      var namespace = param.split('.')
+              , root = namespace.shift()
+              , formParam = root;
+  
+      while (namespace.length) {
+          formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+          param: formParam,
+          msg: msg,
+          value: value
+      };
+    }
+  }));
+
+// Setup express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 //View engine Setup
 app.set('views', path.join(__dirname,'views'));
--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
     database: 'mongodb://localhost/toko-online'
+    // database: 'mongodb://musbahniar:ruangmhs84@ds229438.mlab.com:29438/dblearn'
 }
