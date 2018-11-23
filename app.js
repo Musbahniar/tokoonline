d9e63c61918b5e623c25c37fdd494658e719f1cc
Musbahniar-MNR <ruang.9campus@gmail.com>
2018-11-23 16:57:47 +0700

    pulang dulu ah

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 var express  = require('express');
 var path  = require('path');
 var mongoose = require('mongoose');
-var config = require('./config/database');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var expressValidator = require('express-validator');
+var fileUpload = require('express-fileupload');
+var passport = require('passport');
+var config = require('./config/database');
 
 // Membuat Inisial App
 var app  = express();
+
+
+// Connect to db MongoDB
+mongoose.connect(config.database);
+var db = mongoose.connection;
+
+// If the connection throws an error
+db.on('error',function (err) {  
+    console.log('Mongoose default connection error: ' + err);
+  });
+
+//When the connection is connected
+db.once('open',function(){
+    console.log('Koneksi Ke Database MongoDB Berhasil');
+})
+
+// When the connection is disconnected
+db.on('disconnected', function () {  
+    console.log('Mongoose default connection disconnected'); 
+});
+//-------END Setting Database Mongo
+
+//View engine Setup
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+//Setup Public
+app.use(express.static(path.join(__dirname,'public')));
+
+//setting Global error variabel
+app.locals.error = null;
+
+//Setting express fileUpload middleware
+app.use(fileUpload());
+
 
 //Set MidlleWare Body-Parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -46,40 +83,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-//View engine Setup
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine','ejs');
-
-//Setup Public
-app.use(express.static(path.join(__dirname,'public')));
-
-// Connect to db
-mongoose.connect(config.database);
-var db = mongoose.connection;
-// mongoose.connect('mongodb://localhost/toko-onlinesalah');
-// db.on('error', console.error.bind(console, 'Connection Database Error:'));
-
-// If the connection throws an error
-db.on('error',function (err) {  
-    console.log('Mongoose default connection error: ' + err);
-  });
-
-//When the connection is connected
-db.once('open',function(){
-    console.log('Koneksi Ke Database MongoDB Berhasil');
-})
-
-// When the connection is disconnected
-db.on('disconnected', function () {  
-    console.log('Mongoose default connection disconnected'); 
-  });
-
-
-//Set Server NodeJS
-var port = 3000;
-app.listen(port, function() {
-    console.log('Server running on port 3000');
-});
 
 //Set Router
 var pages = require('./routes/pages.js');
@@ -88,3 +91,9 @@ var adminPages = require('./routes/admin_pages.js');
 // Redirect setup links
 app.use('/', pages);
 app.use('/admin/pages', adminPages);
+
+//Set Server NodeJS
+var port = 3000;
+app.listen(port, function() {
+    console.log('Server running on port 3000');
+});
